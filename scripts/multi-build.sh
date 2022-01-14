47177d1d45da33241907118614cb5ac1d6ea1462
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2022-01-14 11:24:53 +0100

    Update multi-build.sh

--- scripts/multi-build.sh
+++ scripts/multi-build.sh
@@ -7,9 +7,8 @@ function multiBuild(){
     for version in $(jq -r '.apps[] | .version' apps.json); do
         echo "Build Stromae $version"        
         folder="${version//\./'-'}"
-        cd $version
-        PUBLIC_URL="/$folder"
-        yarn && yarn build        
+        cd $version        
+        yarn && PUBLIC_URL=/$folder yarn build        
         cd ..
         mv $version/build build/$folder
     done
