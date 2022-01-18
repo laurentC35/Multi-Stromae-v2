72fe28730e3ce334e04a6a7e920b0929a01456a3
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2022-01-18 17:39:04 +0100

    copy env.sh in multi-env.sh

--- scripts/multi-env.sh
+++ scripts/multi-env.sh
@@ -1,5 +1,34 @@
+
+#!/bin/bash
+function replaceEnvVars(){
+    # Read each line in .env file
+    # Each line represents key=value pairs
+    while read -r line || [[ -n "$line" ]];
+    do
+    # Split env variables by character `=`
+    if printf '%s\n' "$line" | grep -q -e '='; then
+        varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
+        varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
+    fi
+
+    # Read value of current variable if exists as Environment variable
+    value=$(printf '%s\n' "${!varname}")
+    # Otherwise use value from .env file
+    [[ -z $value ]] && value=${varvalue}
+
+    export $varname=$value
+    
+    done < ../.env
+    envsubst < "./configuration.json" > "configuration.temp"
+    envsubst < "./build-configuration.json" > "build-configuration.temp"
+    envsubst < "./oidc.json" > "oidc.temp"
+    mv configuration.temp configuration.json
+    mv build-configuration.temp build-configuration.json
+    mv oidc.temp oidc.json
+}
+
 for app in */ ; do
-    cd $app;
-    bash ../env.sh
+    cd $app
+    replaceEnvVars
     cd ..
 done
